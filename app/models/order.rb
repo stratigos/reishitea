a94355ebbe4fd4309d070ec1c88bab3b77374d51
Stratigos <Stratigos@users.noreply.github.com>
2015-05-25 16:02:17 -0400

    updates class Order validations length from 3 to 2 for several attributes

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,7 +10,7 @@ class Order < ActiveRecord::Base
 
   validates_associated :comment
   validates :name, :street, :city, :state, :country, :postal, :quantity, presence: true
-  validates :name, :city, :state, :country, length: { in: 3..50 }
+  validates :name, :city, :state, :country, length: { in: 2..50 }
   validates :street,   length: { in: 3..100 }
   validates :quantity, numericality: { only_integer: true, greater_than: 0, less_than: 101 }
   validates_format_of :postal, :with => /\d{5}(-\d{4})?/, :message => 'Invalid Postal Code'
