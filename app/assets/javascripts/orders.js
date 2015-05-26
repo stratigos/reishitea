90c29b48a0e6419e3996b7dc840cff7bd93cf972
Stratigos <Stratigos@users.noreply.github.com>
2015-05-26 15:00:59 -0400

    JS for updating orders/index with new Order data (via Pusher subscription)

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,19 +1,72 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-
-// require network-related js here
+/**********************************
+* Require network-related JS here.
+***********************************/
 //= require orders-event-listener
 
-// behavioral js here
+/****************************************
+* Define behavioral JS for Orders below.
+*****************************************/
 
-var updateOrderData = function(data) {
-  console.log('UPDATE ORDER DATA', data);
+/**
+ * Adds a new Order info to the list of recent Orders.
+ * @param order Object
+ *  Expects properties: { name, city, country, quantity }
+ */
+var updateRecentOrdersList = function(order) {
+  // Add new order to the recent orders list.
+  
+  // Container for each Order property to be displayed, set to hidden so it
+  //  can fade in after being added to its parent.
+  var recentDiv = $(document.createElement('div')).addClass('recent-order list-group');
+  recentDiv.html(
+    '<p class="list-group-item list-group-item-success">Name: ' + order.name + '</p>'
+    + '<p class="list-group-item">City: ' + order.city + '</p>'
+    + '<p class="list-group-item">Country: ' + order.country + '</p>'
+    + '<p class="list-group-item">Quantity: ' + order.quantity + '</p>'
+  );
+  recentDiv.hide();
+
+  // Add the recent Order to the top of the list.
+  $('div.recent-orders').prepend($(document.createElement('hr')));
+  $('div.recent-orders').prepend(recentDiv);
+
+  // Fade the new Order in.
+  recentDiv.fadeIn('slow');
+
+  // Fade out and remove least-recent order in the list.
+  $('div.recent-order').last().fadeOut('fast', function() {
+    $(this).remove();
+  });
 }
 
+/**
+ * Increments the 'Reishi Teas Sold' Today and All Time counters.
+ */
+var updateTeasSoldCounts = function() {
+  console.log('incrementing sold counts...');
+}
 
+/**
+ * Takes a 'recent Order' Pusher data JSON as input, and calls functions for
+ *  updating the recent Orders list, and the counts of Orders sold and shipped.
+ * @param data Object
+ *  JSON containing an Order with some metadata.
+ */
+var updateOrderData = function(data) {
+  if (data.order) {
+    updateRecentOrdersList(data.order);
+    updateTeasSoldCounts();
+  }
+}
+
+/**
+ * Check if the orders/index template is loaded, then call the
+ *  appropriate function for subscribing to Pusher events.
+ */
 $(document).on('ready page:load', function() {
-  // Only run the following block if on index page.
   if($('div#orders-index-content')) {
-    subscribeToOrders(updateOrderData);
+    if (typeof subscribeToOrders === 'function') {
+      subscribeToOrders(updateOrderData);
+    }
   }
 });
