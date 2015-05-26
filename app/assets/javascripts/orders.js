459c6df72e8bc5ae809d0e5359b02f63e77bf6b7
Stratigos <Stratigos@users.noreply.github.com>
2015-05-26 00:26:49 -0400

    adds JS to subscribe to Pusher events and log data when recieved

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+// require network-related js here
+//= require orders-event-listener
+
+// behavioral js here
+
+var updateOrderData = function(data) {
+  console.log('UPDATE ORDER DATA', data);
+}
+
+
+$(document).on('ready page:load', function() {
+  // Only run the following block if on index page.
+  if($('div#orders-index-content')) {
+    subscribeToOrders(updateOrderData);
+  }
+});
--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,6 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+// require network-related js here
+
+// behavioral js here
 $(document).on('ready page:load', function() {
   $('.shortorder-form button').on('click', function(e) {
     e.preventDefault();
