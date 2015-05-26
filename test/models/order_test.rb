f36f8fe4f60489c67482fd682f62f57781b4a849
Stratigos <Stratigos@users.noreply.github.com>
2015-05-26 19:10:30 -0400

    new unit test for Order::ship() method

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -10,4 +10,18 @@ class OrderTest < ActiveSupport::TestCase
     assert @order
     assert_not_nil @order.comment
   end
+
+  # Tests Order::ship by setting created_at 4 hours in the past (3 hours
+  #  is needed for shipping to process and take place, its a pretend business
+  #  rule).
+  # Asserts that Order::ship returns TRUE for an instance with the right
+  #  conditions, and that the Order.shipped value is updated. 
+  test 'Can Ship an Order' do
+    @order.created_at = 4.hours.ago
+
+    shipped = @order.ship
+
+    assert shipped
+    assert @order.shipped
+  end
 end
