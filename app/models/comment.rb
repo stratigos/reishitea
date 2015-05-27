a2ebf6aab8cb102d2af3d00de6abfd984ecc1318
Stratigos <Stratigos@users.noreply.github.com>
2015-05-27 01:52:45 -0400

    fixes comment creation bug due to validation of field not massively assigned

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :order, inverse_of: :comment
 
-  validates :order_id, presence: true
-  validates :body,     length: { in: 3..255 }
+  validates :body, length: { in: 3..255 }
 end
