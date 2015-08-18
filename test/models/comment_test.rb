37d0530b0782137a27fc4c370beb75daf9aa18a6
stratigos <stratigos@users.noreply.github.com>
2015-08-18 16:02:39 -0400

    refs #1 Add tests for class Comment;

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,10 +2,28 @@ require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
   setup do
-    @comment = Comment.first
+    @new_comment = Comment.new
   end
 
   test 'Can Select a Comment' do
-    assert @comment
+    assert @one
+  end
+
+  test 'Can Select a Comment\'s Order' do
+    assert @one.order
+  end
+
+  # Test validations. Mainly concerned with enforcing business logic or 
+  #  critical features.
+  test 'Body Length isn\'t too Long' do
+    @new_comment.body = (0...257).map { ('a'..'z').to_a[rand(26)] }.join
+    @new_comment.wont_be :valid?
+    @new_comment.errors[:body].must_equal ['is too long (maximum is 255 characters)']
+  end
+
+  test 'Body Length isn\'t too Short' do
+    @new_comment.body = 'fu'
+    @new_comment.wont_be :valid?
+    @new_comment.errors[:body].must_equal ['is too short (minimum is 3 characters)']
   end
 end
